8e116eb5278dd5ccc0a361caadf9b0b831e3e1eb
pp <pp@gszi.edu.hu>
2022-03-25 10:40:13 +0100

    Add project files.

---
+++ Futoverseny/Resztvevo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Futoverseny
+{
+    class Resztvevo
+    {
+        public string rajtszam;
+        public string nev;
+        public string szuldatum;
+        public string orszag;
+        public double idoperc;
+        public double idomasodperc;
+
+        public Resztvevo(string sor)
+        {
+            string[] t = sor.Split(";");
+            rajtszam = t[0];
+            nev = t[1];
+            szuldatum = t[2];
+            orszag = t[3];
+            idoperc = double.Parse(t[4].Split(":")[0]);
+            idomasodperc = double.Parse(t[4].Split(":")[1]);
+        }
+    }
+}
